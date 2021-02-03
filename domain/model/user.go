f1301c60ff88a13828eb0f5085d92b5339738c7c
Diego Alves <dalveshomeoffice@gmail.com>
2021-02-03 18:31:38 -0300

    Desafio

---
+++ domain/model/user.go
@@ -0,0 +1,39 @@
+package model
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+// Crie uma struct de User com ID (uuid), Name e Email.
+// implemente uma função NewUser que seja capaz de validar que ID, Name e Email são obrigatórios, caso o contrário, ela deve retornar um erro.
+
+type User struct 
+{
+	ID    string `json: "id" valid: "uuid"`
+	Name  string `json: "name" valid: "notnull"`
+	Email string `json: "email" valid: "notnull"`
+	CreatedAt time.Time	`json: "created_at" valid:"-"` 
+}
+
+func NewUser( id string, name string, email string) 
+{
+	user = User
+	{
+		ID:   id,
+		Name: name,
+		Email: email,
+	}
+
+	user.ID = uuid.NewV4().String()
+	user.createdAt = time.Now()
+
+	err := user.isValid()
+
+	if err != nil
+	{
+		return &user, nil
+	}
+}
